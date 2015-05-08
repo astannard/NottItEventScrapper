3b5350f3e7ba2860d666f69459781b8b26992ac2
Andy Stannard <stannarda@gmail.com>
2015-05-08 13:21:34 +0100

    overwrote go code wrong on checked in ops

--- hello.go
+++ hello.go
@@ -1,23 +1,80 @@
 package main
-import "fmt"
+
+import (
+  "log"
+  "strings"
+
+  "github.com/PuerkitoBio/goquery"
+)
+
+
+
 func main() {
-    fmt.Println("go" + "lang")
-    fmt.Println("1+1 =", 1+1)
-    fmt.Println("7.0/3.0 =", 7.0/3.0)
-    fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
-    
-    
-    
-    var a string = "initial"
-    fmt.Println(a)
-    var b, c int = 1, 2
-    fmt.Println(b, c)
-    var d = true
-    fmt.Println(d)
-    var e int
-    fmt.Println(e)
-    f := "short"
-    fmt.Println(f)
+  var groups = getListOfGroups()
+  
+  for _, each := range groups {
+    s := Scrapper(each)
+      s.Scrape()
+  }  
+}
+
+func getListOfGroups()([]Scrapper) {
+  groups := [...]Scrapper{}
+
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/Mobile-Notts/", name: "Mobile Notts"})
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/dotnetnotts/", name: "Dot Net Notts"})
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/Nott-Tuesday-Getting-Nottinghams-tech-scene-together/", name: "Nott Tuesday"})
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/Tech-Nottingham/", name: "Tech Nottingham"})
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/NottinghamProgrammers/", name: "Nottingham Programmers"})
+  groups = append(groups,MeetupGroup{url: "http://www.meetup.com/Nottingham-AWS-Meetup/", name: "Nottingham AWS Meetup"})
+
+  return groups
+}
+
+
+type Scrapper interface {
+
+    Scrape() []Event
+
+}
+
+
+
+type MeetupGroup struct {
+    name, description, url string
+}
+
+type WebAPIGroup struct {
+    name, description, url string
+}
+
+
+type Event struct {
+    groupname string
+    eventname string
+   //date string
+    description string
+   // location string
+}
+
+//this function Area works on the type Rectangle and has the same function signature defined in the interface Shaper.  Therefore, Rectangle now implements the interface Shaper.
+func (g MeetupGroup) Scrape() []Event {
+   
+    events := []Event{}
+    doc, err := goquery.NewDocument(g.url) 
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    doc.Find("#events-list-module .event-list li.event-item").Each(func(i int, s *goquery.Selection) {
+      title := strings.TrimSpace(s.Find("h3").Text())
+      description := strings.TrimSpace(s.Find(".event-desc").Text())
+      var evnt = Event{eventname: title, description: description, groupname:g.name}
+
+      events = append(events,evnt)
+      
+    })
+
+
+     return events
 }
